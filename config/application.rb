baa68c7254bc499c038680f71edc12f478f3a8ad
davidrorez <davidrorez@gmail.com>
2023-06-12 21:55:41 -0600

    First Commit

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module BaseProject2023
+  class Application < Rails::Application
+    config.load_defaults 7.0
+
+    I18n.load_path += Dir[Rails.root.join("config", "locales", "*.{yml}")]
+    I18n.default_locale = :es
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
+  end
+end
