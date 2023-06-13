3b48be575a843741420fcd48508aca6d04676b25
davidrorez <davidrorez@gmail.com>
2023-06-13 14:57:37 -0600

    Navbar and Sidebar done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tests
+  root "dashboards#index"
 end
