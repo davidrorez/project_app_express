faac0571aa93173c449db0e39ee78900aaa3ef52
fabriciof24 <fabricio.fonseca@ucrso.info>
2023-06-26 16:39:46 -0600

    FFS-B83033 Proyect WIP

---
+++ app/controllers/api/dishes_controller.rb
@@ -0,0 +1,49 @@
+module Api
+    class DishesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :set_dish, only: %i[ show edit update destroy ]
+      
+      def index
+        @dishes = Dish.all
+      end
+      
+      def show; end
+      
+      def new
+        @dishe = Dish.new
+      end
+    
+      def edit; end
+      
+      def create
+        @dish = Dish.new(dish_params)
+          if @dish.save
+            render 'api/dishes/show', status: :created
+          else
+            render json: @dish.errors, status: :unprocessable_entity
+          end
+      end
+      
+      def update
+          if @dish.update(dish_params)
+            render 'api/dishes/show', status: :ok
+          else
+            render json: @dish.errors, status: :unprocessable_entity
+          end
+      end
+      
+      def destroy
+        @dish.destroy
+        render 'api/dishes/show', status: :ok
+      end
+      
+      private
+        def set_dish
+          @dish = Dish.find(params[:id])
+        end
+      
+        def dish_params
+          params.require(:dish).permit(:name, :description, :price, :photo, :state)
+        end
+    end
+  end
